34fd4c8e96baba9ed7915e2c3b22246d1404ffaf
Nikita Sukhodoev <dogmach94@yandex.ru>
2023-01-23 21:01:58 +0400

    add files

--- src/main/java/Application_3.java
+++ src/main/java/Application_3.java
@@ -6,31 +6,33 @@ public class Application_3 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите размер массива: ");
 
-        if (scanner.hasNextInt()) {
-            int size = scanner.nextInt();
-            int array[] = new int[size];
+        while (scanner.hasNext()) {
+            if (scanner.hasNextInt()) {
+                int size = scanner.nextInt();
+                int[] array = new int[size];
+                System.out.println("Введите значения элементов массива: ");
 
-            System.out.println("Вводите значения элементов массива: ");
-
-            for (int i = 0; i < array.length; i++) {
-                if (scanner.hasNextInt()) {
-                    array[i] = scanner.nextInt();
-                } else {
-                    scanner.close();
+                for (int i = 0; i < array.length; i++) {
+                    if (scanner.hasNextInt()) {
+                        array[i] = scanner.nextInt();
+                    } else {
+                        System.out.println("Вы ввели неверный символ, введите число заново: ");
+                        i--;
+                        scanner.next();
+                    }
                 }
-            }
-
-            /* Вывод элементов массива кратные числу 3 */
-
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] % 3 == 0) {
-                    System.out.println(array[i]);
+                /* Вывод элементов массива кратные числу 3 */
+                for (int j : array) {
+                    if (j % 3 == 0) {
+                        System.out.println(j);
+                    }
                 }
+            } else {
+                System.out.println("Введёное вами значение не является числом.");
+                System.out.println("Введите число заново: ");
+                scanner.next();
             }
-            scanner.close();
         }
-        else {
-            System.out.println("Введёное вами значение не является числом.");
-        }
+        scanner.close();
     }
 }
